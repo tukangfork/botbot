470b0c64df9542a8e8d38e8bf59923e6c00ab83a
Reyzu <reyzuproject@gmail.com>
2022-09-18 03:56:30 +0700

    Update log_channel.py

--- ReyzuRobot/modules/log_channel.py
+++ ReyzuRobot/modules/log_channel.py
@@ -197,14 +197,14 @@ if is_module_loaded(FILENAME):
 
 
     __help__ = """
-──「 Log channel 」──
-❧ /logchannel*:* get log channel info
-❧ /setlog*:* set the log channel.
-❧ /unsetlog*:* unset the log channel.
-*Setting the log channel is done by*:
-➩ adding the bot to the desired channel (as an admin!)
-➩ sending /setlog in the channel
-➩ forwarding the /setlog to the group
+Log channel
+❧ /logchannel - get log channel info
+❧ /setlog - set the log channel.
+❧ /unsetlog - unset the log channel.
+*Setting the log channel is done by* :
+❧ adding the bot to the desired channel (as an admin!)
+❧ sending /setlog in the channel
+❧ forwarding the /setlog to the group
 """
 
     __mod_name__ = "Log Channel​"
