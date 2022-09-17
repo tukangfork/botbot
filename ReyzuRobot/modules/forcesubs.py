6352f53dcccaefa17d06da97d83deeb2ad86a5db
Reyzu <reyzuproject@gmail.com>
2022-09-18 03:35:04 +0700

    Update forcesubs.py

--- ReyzuRobot/modules/forcesubs.py
+++ ReyzuRobot/modules/forcesubs.py
@@ -198,10 +198,10 @@ Everything is fun, until a spammer starts entering your group, and you have to b
 But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups?\n
 *No longer!* With Federation, you can make a ban in one chat overlap with all other chats.\n
 You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect.\n
-*Commands:*\n
+*Commands :*\n
 Feds are now divided into 3 sections for your ease.
-❧ `/fedownerhelp`*:* Provides help for fed creation and owner only commands
-❧ `/fedadminhelp`*:* Provides help for fed administration commands
-❧ `/feduserhelp`*:* Provides help for commands anyone can use
+❧ `/fedownerhelp` - Provides help for fed creation and owner only commands
+❧ `/fedadminhelp` - Provides help for fed administration commands
+❧ `/feduserhelp` - Provides help for commands anyone can use
 """
 __mod_name__ = "F-Sub/Feds"
