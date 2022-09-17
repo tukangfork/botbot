96465fa5005d1355e6c5daedd4eea999aa30983c
Reyzu <reyzuproject@gmail.com>
2022-09-18 03:30:04 +0700

    Update disable.py

--- ReyzuRobot/modules/disable.py
+++ ReyzuRobot/modules/disable.py
@@ -318,13 +318,13 @@ if is_module_loaded(FILENAME):
 
 
     __help__ = """
-❂ /cmds*:* check the current status of disabled commands
-*Admins only:*
-❂ /enable <cmd name>*:* enable that command
-❂ /disable <cmd name>*:* disable that command
-❂ /enablemodule <module name>*:* enable all commands in that module
-❂ /disablemodule <module name>*:* disable all commands in that module
-❂ /listcmds*:* list all possible toggleable commands
+❧ /cmds - check the current status of disabled commands
+*Admins only :*
+❧ /enable <cmd name> - enable that command
+❧ /disable <cmd name> - disable that command
+❧ /enablemodule <module name> - enable all commands in that module
+❧ /disablemodule <module name> - disable all commands in that module
+❧ /listcmds - list all possible toggleable commands
 """
 
     DISABLE_HANDLER = CommandHandler("disable", disable, run_async=True)
