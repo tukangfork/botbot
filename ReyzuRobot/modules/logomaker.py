d2f2c2d81332297fb7baf0ca8a26f63e534aded7
forktukang <102500858+forktukang@users.noreply.github.com>
2022-09-18 10:17:15 +0700

    Update logomaker.py

--- ReyzuRobot/modules/logomaker.py
+++ ReyzuRobot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ReyzuRobot](https://t.me/ReyzuRobot)",
+            caption="Logo by [Ochobot](https://t.me/BotOchobot)",
             force_document=False,
         )
         os.remove(flnme)
