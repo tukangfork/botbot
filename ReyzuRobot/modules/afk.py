bdfd11efc05d8d4a7813eb4271efa3e5d8a89641
REYZU <reyzuproject@gmail.com>
2022-09-23 14:33:23 +0700

    Update afk.py

--- ReyzuRobot/modules/afk.py
+++ ReyzuRobot/modules/afk.py
@@ -38,14 +38,14 @@ def afk(update: Update, context: CallbackContext):
         reason = args[1]
         if len(reason) > 100:
             reason = reason[:100]
-            notice = "\nYour afk reason was shortened to 100 characters."
+            notice = "\nAlasan afk Anda dipersingkat menjadi 100 karakter."
     else:
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
+        update.effective_message.reply_text("{} sekarang telah AFK!{}".format(fname, notice))
     except BadRequest:
         pass
 
@@ -64,14 +64,14 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
-                "Where is {}?\nIn the chat!",
+                "{} di sini!",
+                "{} kembali!",
+                "{} sekarang dalam obrolan!",
+                "{} sudah bangun!",
+                "{} sudah kembali online!",
+                "{} akhirnya di sini!",
+                "Selamat datang kembali! {}",
+                "Dimana {}?\n Dalam obrolan!",
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
